265955386515d5021b6edf25baa7eb1862acb6b2
ALIS <tatu.tammi@edu.turkuamk.fi>
2025-01-25 09:23:03 +0200

    Added footer jsx to components

---
+++ website/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Easywayfare. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
