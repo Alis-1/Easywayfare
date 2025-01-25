0a05e531c43e1b0cc4c5113faed8421187cca7ce
ALIS <tatu.tammi@edu.turkuamk.fi>
2025-01-25 10:30:39 +0200

    Header: Added cool airplane "logo" back

--- website/src/components/Header.jsx
+++ website/src/components/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import '../styles/header.css';
+import logo from '../assets/lentokone.png';
 
 const Header = () => {
   return (
     <div className="header-background-card">
-    <div className="header">
-      <h1>Easywayfare</h1>
+      <div className="header">
+        <h1>Easywayfare</h1>
+        <img src={logo} className="logo react" alt="React logo" />
         <div className="nav">
-        <ul>
-          <a href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
-        </ul>
+          <ul>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+          </ul>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
