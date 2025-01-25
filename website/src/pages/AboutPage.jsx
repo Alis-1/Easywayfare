76dc3e53cd335a8d26bbb43fac6e5a21e1f2ef4d
ALIS <tatu.tammi@edu.turkuamk.fi>
2025-01-25 09:24:46 +0200

    Src: Added about page if necessary

---
+++ website/src/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className="about-page">
+      <h1>About Easywayfare</h1>
+      <p>Easywayfare is your go-to source for the best travel tips and advice.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
