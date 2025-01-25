356e0effe3d4d30c92fe73b3a2113674956f9eaa
ALIS <tatu.tammi@edu.turkuamk.fi>
2025-01-25 18:07:35 +0200

    Added nav.jsx

---
+++ website/src/components/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles/nav.css';
+
+const Nav = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">Web-sovellukseni</div>
+            <ul className="navbar-menu">
+                <li><a href="#home">Koti</a></li>
+                <li><a href="#about">Tietoa</a></li>
+                <li><a href="#contact">Yhteystiedot</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
