fa94f7a15cc1dfbc302634dacc8fa3716c2cf125
Jesse Sintonen <sintonej.esse@gmail.com>
2025-02-19 15:48:37 +0200

    Map working properly
    Fixed issue when the Log Address button had to be double clicked to get coordinates. Also changed it so you can move the map with the mouse again.

--- website/src/components/GoogleMap.jsx
+++ website/src/components/GoogleMap.jsx
@@ -1,20 +1,31 @@
 import api_key from "../../apiFolder/api"
 import {APIProvider, Map} from '@vis.gl/react-google-maps';
 import "../styles/Map.css"
+import { useEffect, useState } from "react";
 const API_KEY = api_key;
 
 
 const GoogleMapShow = ({latitude, longitude}) => {
+
+    const [mapCenter, setMapCenter] = useState({lat: latitude, lng: longitude})
+
+    useEffect(() => {
+      setMapCenter({ lat: latitude, lng: longitude})
+    }, [latitude,longitude])
+
+    const handleCameraChanged = (event) => {
+      const newCenter = event.detail.center
+      setMapCenter(newCenter)
+    }
 
     return (
         <APIProvider apiKey={API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
         <div style={{ height: '50vh', width: '100%' }}>
         <Map
         defaultZoom={13.4}
-        center={{lat: latitude, lng: longitude}}
-        onCameraChanged={(ev) =>
-          console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
-        }>
+        center={mapCenter}
+        onCameraChanged= {handleCameraChanged}
+        >
         </Map>
         
         </div>
--- website/src/components/SearchBarGoogle.jsx
+++ website/src/components/SearchBarGoogle.jsx
@@ -42,24 +42,25 @@ const SearchBarGoogle = () => {
       const selectedAddress = picker.value.formattedAddress
       console.log("Selected address: ", selectedAddress)
       setAddress(selectedAddress)
+      
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${selectedAddress},&key=${API_KEY}`, {
+        method: 'GET',
+      })
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === 'OK' && data.results.length > 0) {
+          const latitude = data.results[0].geometry.location.lat
+          const longitude = data.results[0].geometry.location.lng
+          setLatitude(latitude)
+          setLongitude(longitude)
+        } else {
+          console.error("No results found or invalid response: ", data.status)
+        }
+      })
     }
     else {
       console.log("No address selected")
     }
-    fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address},&key=${API_KEY}`, {
-      method: 'GET',
-    })
-    .then(res => res.json())
-    .then(data => {
-      if (data.status === 'OK' && data.results.length > 0) {
-        const latitude = data.results[0].geometry.location.lat
-        const longitude = data.results[0].geometry.location.lng
-        setLatitude(latitude)
-        setLongitude(longitude)
-      } else {
-        console.error("No results found or invalid response: ", data.status)
-      }
-    })
     console.log(latitude)
     console.log(longitude)
   }
